08235bc3bafb58686b303b05e3d55394abff3c86
PaulBrust <pbrust90@gmail.com>
2016-07-21 20:07:46 +0200

    checkboxes halb fertig, performance tot

--- model.py
+++ model.py
@@ -4,21 +4,30 @@ from stats import Stats
 
 
 class Model(object):
-    def __init__(self, inputNames, outputNames, data):
-        self.inputNames = inputNames
-        self.outputNames = outputNames
-        self.data = data
-        self.stats = [Stats(col) for col in data.T]
-        self.corrmat = numpy.corrcoef(data.T)
-        self.activeVariables = self.inputNames + [outputNames[1]]
+	def __init__(self, inputNames, outputNames, data):
+		self.inputNames = inputNames
+		self.outputNames = outputNames
+		self.data = data
+		self.stats = [Stats(col) for col in data.T]
+		self.corrmat = numpy.corrcoef(data.T)
+		self.activeVariables = self.inputNames + self.outputNames
+		if len(self.activeVariables)>7:
+			self.activeVariables=self.activeVariables[:7]
+	
+	def setActiveVar(self,name):
+		if not name in self.activeVariables:
+			self.activeVariables.append(name)
 
+	def setPassiveVar(self,name):
+		if name in self.activeVariables:
+			self.activeVariables.remove(name)
 
 def fromFile(filename):
-    with open(filename) as f:
-        header = f.readline()[:-1]
-    input = header.split(";")[0]
-    input = input.split(",")
-    output = header.split(";")[1]
-    output = output.split(",")
-    data = numpy.genfromtxt(filename, delimiter=",", skip_header=1)
-    return Model(input, output, data)
+	with open(filename) as f:
+		header = f.readline()[:-1]
+	input = header.split(";")[0]
+	input = input.split(",")
+	output = header.split(";")[1]
+	output = output.split(",")
+	data = numpy.genfromtxt(filename, delimiter=",", skip_header=1)
+	return Model(input, output, data)
