be4067b3dac41813723add53ce1c5413ba266c42
Paul Debus <debuspaul@gmail.com>
2016-08-07 16:48:07 +0200

    first steps with pyqtgraph

--- main.py
+++ main.py
@@ -1,70 +1,81 @@
 from PyQt4.uic import loadUiType
 from PyQt4.uic import loadUi
-from PyQt4.QtGui import *
+from pyqtgraph.Qt import QtGui
 import plots
 
 
 Ui_MainWindow, QMainWindow = loadUiType('GUI/MainWindow.ui')
 
+
 class Main(QMainWindow, Ui_MainWindow):
 
-	def __init__(self, model):
-		self.model=model
-		super(Main, self).__init__()
-		self.setupUi(self)
-		self.splitter.splitterMoved.connect(self.resizeLeft)
-		loadUi('GUI/mat.ui', self.matrixWindow)
+    def __init__(self, model):
+        self.model = model
+        super(Main, self).__init__()
+        self.setupUi(self)
+        self.splitter.splitterMoved.connect(self.resizeLeft)
+        loadUi('GUI/mat.ui', self.matrixWindow)
 
-	def addMatrix(self):
-		size = self.variableList.frameGeometry().width() / len(self.model.activeVariables)
-		num = len(model.activeVariables)
-		for row in range(1,num+1):
-			for col in range(row,num+1):
-				if row == col:
-					plot = plots.histogram(self.model, variables = row,
-						parent=None, width = size, height= size)
-				else:
-					plot = plots.scatterplot(self.model, variables = [row+2,col+2],
-						parent=None, width = size, height= size)
-				self.matrixWindow.gridLayout.addWidget(plot,num+1-row,col)
-		size = self.splitter.size().width()
-		self.splitter.setSizes([200, size-200])
-		self.resizeLeft()
+    def addMatrix(self):
+        while self.matrixWindow.gridLayout.count():
+            item = self.matrixWindow.gridLayout.takeAt(0)
+            widget = item.widget()
+            # if widget has some id attributes you need to
+            # save in a list to maintain order, you can do that here
+            # i.e.:   aList.append(widget.someId)
+            widget.deleteLater()
+        size = self.variableList.frameGeometry(
+            ).width() / len(self.model.activeVariables)
+        num = len(model.activeVariables)
+        print(model.activeVariables)
+        for row in range(1, num+1):
+            for col in range(row, num+1):
+                var1 = (model.activeVariables[row-1], {})
+                var2 = (model.activeVariables[col-1], {})
+                if row == col:
+                    plot = plots.histogram(self.model, variables=[var1], parent=None, width=size, height=size)
+                else:
+                    plot = plots.scatterplot(self.model, variables=[var1, var2],
+                                            parent=None, width=size, height=size)
+                self.matrixWindow.gridLayout.addWidget(plot,num+1-row,col)
+        size = self.splitter.size().width()
+        self.splitter.setSizes([200, size-200])
+        self.resizeLeft()
 
-	def loadNames(self):
-		qmodel = QStandardItemModel(self.variableList)
-		qmodel.itemChanged.connect(self.listChange)
-		#QObject.connect(qmodel,SIGNAL('selectionChanged()',listChange))
-		variables = self.model.inputNames + self.model.outputNames
-		for var in variables:
-			item = QStandardItem(var)
-			item.setCheckable(True)
-			if var in self.model.activeVariables:
-				item.setCheckState(2)
-			qmodel.appendRow(item)
-		self.variableList.setModel(qmodel)
+    def loadNames(self):
+        qmodel = QtGui.QStandardItemModel(self.variableList)
+        qmodel.itemChanged.connect(self.listChange)
+        # QObject.connect(qmodel,SIGNAL('selectionChanged()',listChange))
+        variables = self.model.inputNames + self.model.outputNames
+        for var in variables:
+            item = QtGui.QStandardItem(var)
+            item.setCheckable(True)
+            if var in self.model.activeVariables:
+                item.setCheckState(2)
+            qmodel.appendRow(item)
+        self.variableList.setModel(qmodel)
 
-	def listChange(self,e):
-		print(e.text(),e.checkState())
-		if e.checkState==0:
-			self.model.setPassiveVar(e.text())
-		else:
-			self.model.setActiveVar(e.text())
-		self.addMatrix()
-	
-	def resizeLeft(self):
-		size = self.variableList.frameGeometry().width()
-		self.matrixWindow.resize(size,size)
+    def listChange(self,e):
+        print(e.text(),e.checkState())
+        if e.checkState()==0:
+            self.model.setPassiveVar(e.text())
+        else:
+            self.model.setActiveVar(e.text())
+        self.addMatrix()
+
+    def resizeLeft(self):
+        size = self.variableList.frameGeometry().width()
+        self.matrixWindow.resize(size,size)
 
 if __name__ == '__main__':
-	import sys
-	from PyQt4 import QtGui
-	import model
+    import sys
+    from PyQt4 import QtGui
+    import model
 
-	model = model.fromFile('output.txt')
-	app = QtGui.QApplication(sys.argv)
-	main = Main(model)
-	main.addMatrix()
-	main.loadNames()
-	main.show()
-	sys.exit(app.exec_())
+    model = model.fromFile('output.txt')
+    app = QtGui.QApplication(sys.argv)
+    main = Main(model)
+    main.addMatrix()
+    main.loadNames()
+    main.show()
+    sys.exit(app.exec_())
--- plots.py
+++ plots.py
@@ -1,73 +1,30 @@
-import matplotlib.pyplot
-from matplotlib.figure import Figure
-from PyQt4 import QtGui, QtCore
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import pyqtgraph as pg
 
-scheme='summer'
-
-class MyMplCanvas(FigureCanvas):
-
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-
-    def __init__(
-            self,
-            model,
-            variables,
-            labels=False,
-            parent=None,
-            width=5,
-            height=4,
-            dpi=100):
-        self.model = model
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
-        if not labels:
-            self.axes.tick_params(
-                axis='both',
-                labelbottom='off',
-                labelleft='off')
-            fig.subplots_adjust(
-                left=0.01,
-                bottom=0.01,
-                right=0.99,
-                top=0.99,
-                wspace=None,
-                hspace=None)
-
-        self.compute_initial_figure(variables)
-
-        #
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                                   QtGui.QSizePolicy.Expanding,
-                                   QtGui.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-
-    def compute_initial_figure(self):
-        pass
+scheme = 'summer'
 
 
-class histogram(MyMplCanvas):
+def histogram(model, variables, parent=None, width=4, height=4):
+    pw = pg.ScatterPlotWidget()
+    pw.setData(model.data)
+    pw.setFields(variables)
+    return pw
 
-    def compute_initial_figure(self, variables):
-        x = self.model.data[:, variables]
-        self.axes.hist(x, 20, normed=1, color=matplotlib.pyplot.get_cmap(scheme)(0.5))
 
-
-class scatterplot(MyMplCanvas):
-
-    def compute_initial_figure(self, variables):
-        t = self.model.data[:, variables[0]]
-        s = self.model.data[:, variables[1]]
-        self.axes.scatter(t, s, c='black',marker='.',alpha=0.2)
-        corr = self.model.corrmat[variables[0], variables[1]]
-        self.axes.set_axis_bgcolor(correlationColor(corr))
+def scatterplot(model, variables, parent=None, width=4, height=4):
+    pw = pg.PlotWidget()
+#    pw.setData(model.data)
+ #   pw.setFields(variables)
+    n = 300
+    s2 = pg.ScatterPlotItem(size=10, pen=pg.mkPen('w'), pxMode=True)
+    pos = np.random.normal(size=(2,n), scale=1e-5)
+    spots = [{'pos': pos[:,i], 'data': 1, 'brush':pg.intColor(i, n), 'symbol': 'o', 'size': 1} for i in range(n)]
+    s2.addPoints(spots)
+    pw.addItem(s2)
+    return pw
 
 
 def correlationColor(corr):
-    summer =  matplotlib.pyplot.get_cmap(scheme)
+    summer = matplotlib.pyplot.get_cmap(scheme)
     return summer(abs(corr))
